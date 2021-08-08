37bfa03a4be398f76b6e2bfcc860707e75ea7511
Minsik Kim <pro.mskim0215@gmail.com>
2021-08-08 21:53:09 +0900

    binding 실습

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/databinding">Data Binding</router-link> | 
+    <router-link to="/databindinghtml">Data Binding Html</router-link> | 
+    <router-link to="/databindinginputtext">Data Binding InputText</router-link> |
+    <router-link to="/databindinginputnumber">Data Binding InputNumber</router-link> |
+    <router-link to="/databindingtextarea">Data Binding Textarea</router-link> |
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +18,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
